26644eed67ebf580734a1413ba8a936ebb8e10f3
Aditya Vekhande <adityavekhande@gmail.com>
2017-09-20 15:02:45 +0530

    added endpoints for UserFilteredTournamentView and TournamentPairingsView

--- tournament_api/urls.py
+++ tournament_api/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from .views import (PlayerListView, PlayerDetailView, TournamentListView,
                     TournamentDetailView, MatchListView, MatchDetailView,
                     RoundListView, RoundDetailView, PlayerRegisterListView,
-                    TournamentStandingsView, RoundPairingsView, TournamentRoundView)
+                    TournamentStandingsView, TournamentPairingsView, TournamentRoundView,
+                    UserFilteredTournamentView)
 
 
 urlpatterns = [
@@ -13,9 +14,10 @@ urlpatterns = [
     url(r'tournaments/(?P<pk>[\w-]+)/$', TournamentDetailView.as_view(), name='tournament_detail'),
     url(r'tournaments/(?P<pk>[\w-]+)/get-standings/$', TournamentStandingsView.as_view(), name='get_standings'),
     url(r'tournaments/(?P<pk>[\w-]+)/round/(?P<round_num>[\w-]+)/$', TournamentRoundView.as_view(), name='tournament_round'),
+    url(r'tournaments/user/(?P<pk>[\w-]+)', UserFilteredTournamentView.as_view(), name='user_tournaments'),
     url(r'matches/$', MatchListView.as_view(), name='match_list'),
     url(r'^matches/(?P<pk>[\w-]+)', MatchDetailView.as_view(), name='match_detail'),
     url(r'rounds/$', RoundListView.as_view(), name='round_list'),
     url(r'rounds/(?P<pk>[\w-]+)/$', RoundDetailView.as_view(), name='round_detail'),
-    url(r'rounds/(?P<pk>[\w-]+)/get-pairings', RoundPairingsView.as_view(), name='get_pairings')
+    url(r'tournaments/(?P<pk>[\w-]+)/get-pairings', TournamentPairingsView.as_view(), name='get_pairings')
 ]
